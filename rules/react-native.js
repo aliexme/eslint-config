31c63a597da743f685bfeef64d8d4f824828785d
Alexander Smirnov <al.smirnov996@gmail.com>
2023-05-13 13:23:03 +0300

    apply react rules for all files

--- rules/react-native.js
+++ rules/react-native.js
@@ -1,4 +1,15 @@
 module.exports = {
+  "env": {
+    "react-native/react-native": true,
+  },
+  "plugins": ["react-native"],
+  "extends": ["plugin:import/react-native"],
+  "rules": {
+    "react-native/no-unused-styles": "error",
+    "react-native/no-inline-styles": "error",
+    "react-native/no-raw-text": ["error", { "skip": ["Text", "Button"] }],
+    "react-native/no-single-element-style-arrays": "error",
+  },
   "overrides": [
     {
       "files": ["**/*.{jsx,tsx}"],
@@ -6,17 +17,6 @@ module.exports = {
         "ecmaFeatures": {
           "jsx": true,
         },
-      },
-      "env": {
-        "react-native/react-native": true,
-      },
-      "plugins": ["react-native"],
-      "extends": ["plugin:import/react-native"],
-      "rules": {
-        "react-native/no-unused-styles": "error",
-        "react-native/no-inline-styles": "error",
-        "react-native/no-raw-text": ["error", { "skip": ["Text", "Button"] }],
-        "react-native/no-single-element-style-arrays": "error",
       },
     },
   ],
